e114e3505a1d965e4bb676020208975bf9f6498d
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-10-30 19:14:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ console.log("log");
 var prefix = "$";
 
 client.on('ready', () => {
-    client.user.setActivity(".ticket||.close ",{type: 'WATCHING'})
+    client.user.setActivity(".new||.close ",{type: 'WATCHING'})
 
 });
 
@@ -56,7 +56,7 @@ client.on("message", (message) => {
  
         message.channel.send(`هل أنت متأكد؟ بعد التأكيد ، لا يمكنك عكس هذا الإجراء!\n للتأكيد ، اكتب\`$confirm\`. سيؤدي ذلك إلى مهلة زمنية في غضون 10 ثوانٍ وإلغائها`)
             .then((m) => {
-                message.channel.awaitMessages(response => response.content === '$confirm', {
+                message.channel.awaitMessages(response => response.content === '.confirm', {
                         max: 1,
                         time: 10000,
                         errors: ['time'],
